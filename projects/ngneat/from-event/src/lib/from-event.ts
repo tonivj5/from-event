69d53b5b3a01b80d40b8e3dca10f6b4151f4b5d9
tonivj5 <tonivj5@gmail.com>
2020-05-11 12:33:56 +0200

    fix: memory leak, force unsubscription

--- projects/ngneat/from-event/src/lib/from-event.ts
+++ projects/ngneat/from-event/src/lib/from-event.ts
@@ -2,23 +2,25 @@ import { ElementRef } from '@angular/core';
 import { fromEvent, defer } from 'rxjs';
 
 import { initIfNeeded } from './init-if-needed';
+import { takeUntil } from 'rxjs/operators';
 
 export function FromEvent<K extends keyof DocumentEventMap>(event: K, eventOptions?: AddEventListenerOptions) {
   return function (target: any, propertyKey: string) {
     const eventToken = Symbol(propertyKey);
+    const destroyToken = Symbol(propertyKey);
 
     Object.defineProperty(target, propertyKey, {
       set(elementRef: ElementRef) {
         const event$ = fromEvent(elementRef.nativeElement, event, eventOptions);
 
         if (this[eventToken]) {
-          event$.subscribe(this[eventToken]);
+          event$.pipe(takeUntil(this[destroyToken])).subscribe(this[eventToken]);
         }
 
         this[eventToken] = event$;
       },
       get() {
-        initIfNeeded(this, eventToken);
+        initIfNeeded(this, eventToken, destroyToken);
 
         return defer(() => this[eventToken]);
       },
--- projects/ngneat/from-event/src/lib/init-if-needed.ts
+++ projects/ngneat/from-event/src/lib/init-if-needed.ts
@@ -1,14 +1,19 @@
 import { Subject } from 'rxjs';
 import { finalize, refCount, publish } from 'rxjs/operators';
 
-export function initIfNeeded(that: object, eventToken: symbol) {
+export function initIfNeeded(that: object, eventToken: symbol, destroyToken?: symbol) {
   if (!that[eventToken]) {
+    that[destroyToken] = new Subject();
     let event$ = new Subject();
 
     that[eventToken] = event$.pipe(
       finalize(() => {
         event$.complete();
         event$ = null;
+
+        that[destroyToken].next();
+        that[destroyToken].complete();
+        that[destroyToken] = null;
       }),
       publish(),
       refCount()
